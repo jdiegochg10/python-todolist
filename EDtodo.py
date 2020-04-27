d6dc0c73571eba0fc9094c4b75c7a29023edac9b
Diego <diego_jd_28@hotmail.com>
2020-04-27 15:59:51 -0500

    first class

---
+++ EDtodo.py
@@ -0,0 +1,28 @@
+def printMenu():
+  print("************************************")
+  print("**EDTODO****************************")
+  print("************************************")
+  print("Seleccione una opcion")
+  print("* 1. Agregar tarea")
+  print("* 2. Editar tarea")
+  print("* 3. Marcar como hecha")
+  print("* 4 Borrar tarea")
+  print("* 5. Listar tarea")
+  print("* 6. Salir")
+  
+printMenu()
+
+  #opcion = input("* Digite la opcion ")
+  #print ("usted ha digitado")
+  #print (opcion)
+  #print("Agragando una tarea: ")
+
+def printTitleList():
+  print("")
+  print("**************************************")
+  print("**************************************")
+  print("* LISTADO DE TAREAS ******************")
+  print("**************************************")
+  print("")
+
+printTitleList()
